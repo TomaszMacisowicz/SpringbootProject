247a4719a759e4a5ecc13a3e9214c00069868b12
TomaszMacisowicz <TomaszMacisowicz@users.noreply.github.com>
2017-07-27 18:48:43 +0200

    first commit

---
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.entity;
+
+/**
+ *
+ * @author MAC
+ */
+public class StudentEntity {
+
+    private Long id;
+    private String name, surname, course;
+
+    //pole - stpendnium
+    public StudentEntity() {
+
+    }
+
+    public StudentEntity(Long id, String name, String surname, String course) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        StudentEntity that = (StudentEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentEntity{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", surname='" + surname + '\''
+                + ", course='" + course + '\''
+                + '}';
+    }
+}
